0693c705eb324c05dcfe54c0854f45cba5214af1
Guilherme <guigfaria01@gmail.com>
2022-12-15 12:20:21 -0300

    Matcher para comparação de conteudo (match, start_with e end_with)

---
+++ spec/matchers/comparacao/conteudo.rb
@@ -0,0 +1,19 @@
+describe "Comparação de Conteúdo" do
+  context "Regex" do
+    it '#match' do
+      expect("fulano@mail.com").to match(/..@../)
+    end
+  end
+  
+  context "Começa ou termina com" do
+    it '#start_with' do
+      expect("Fulano de Tal").to start_with("Fulano")
+      expect([1,2,3]).to start_with(1)
+    end
+  
+    it '#end_with' do
+      expect("Fulano de Tal").to end_with("Tal")
+      expect([1,2,3]).to end_with(3)
+    end
+  end
+end
